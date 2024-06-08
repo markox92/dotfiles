495a1633a5827a21a190d76fcc91c36304bf763f
markox92 <159203552+markox92@users.noreply.github.com>
2024-06-08 20:50:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,13 +16,20 @@ sudo pacman -S --noconfirm firefox kitty xsettingsd wget curl nano zip unzip ttf
 yay -S --noconfirm rofi-lbonn-wayland-git nwg-dock-hyprland waybar-git visual-studio-code-bin
 
 # Install Theme,cursors and icons
-cd ~ && git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git --depth=1 && cd WhiteSur-gtk-theme && ./install.sh -l && ./tweaks.sh -f && cd.. && rm -rf WhiteSur-gtk-theme
-cd ~ && git clone https://github.com/vinceliuice/McMojave-cursors && cd McMojave-cursors && sudo ./install.sh && cd.. && rm -rf McMojave-cursors
-cd ~ && git clone https://github.com/yeyushengfan258/Reversal-icon-theme.git && cd Reversal-icon-theme && ./install.sh -a && cd.. && rm -rf Reversal-icon-theme
+cd ~ && git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git --depth=1 && cd WhiteSur-gtk-theme && ./install.sh -l && cd .. && rm -rf WhiteSur-gtk-theme
+cd ~ && git clone https://github.com/vinceliuice/McMojave-cursors && cd McMojave-cursors && sudo ./install.sh && cd .. && rm -rf McMojave-cursors
+cd ~ && git clone https://github.com/yeyushengfan258/Reversal-icon-theme.git && cd Reversal-icon-theme && ./install.sh -a && cd .. && rm -rf Reversal-icon-theme
 
-# Copy files
-cd ~ && git clone https://github.com/markox92/dotfiles && cd dotfiles && cp -r .config ~/ && cp -r Wallpapers ~/
-mkdir ~/.fonts && git clone https://github.com/epk/SF-Mono-Nerd-Font ~/.fonts && git clone https://github.com/sahibjotsaggu/San-Francisco-Pro-Fonts ~/.fonts && fc-cache -f -v
+# Install fonts
+mkdir ~/.fonts 
+git clone https://github.com/epk/SF-Mono-Nerd-Font ~/.fonts 
+git clone https://github.com/sahibjotsaggu/San-Francisco-Pro-Fonts ~/.fonts 
+fc-cache -f -v
+
+# Copy dotfiles
+cd ~/dotfiles
+cp -r .config ~/ && cp -r Wallpapers ~/
+cd .. && rm -rf dotfiles
 
 function config_kernel() {
     clear
