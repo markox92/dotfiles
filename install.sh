b1acc8c40dc60b8546bb3f36d86c714725c48f04
markox92 <159203552+markox92@users.noreply.github.com>
2024-06-09 13:02:43 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ cp -r .config ~/ && cp -r Wallpapers ~/
 function config_kernel() {
     printf "Config kernel..."
     sudo sed -Ei 's/^(MODULES=\([^\)]*)\)/\1nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
-    sudo echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf
+    sudo echo -e "options nvidia-drm modeset=1 NVreg_RegistryDwords=\"PowerMizerEnable=0x1; PerfLevelSrc=0x2222; PowerMizerLevel=0x3; PowerMizerDefault=0x3; PowerMizerDefaultAC=0x3\"" | sudo tee -a /etc/modprobe.d/nvidia.conf
     sudo mkinitcpio -P
     if [ -f /etc/default/grub ]; then
         sudo sed -i 's/\(GRUB_CMDLINE_LINUX_DEFAULT=".*\)"/\1 nvidia_drm.modeset=1 nvidia.NVreg_PreserveVideoMemoryAllocations=1"/' /etc/default/grub
