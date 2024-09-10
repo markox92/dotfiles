2b21cd41d1cc51083b6c296a2891b3bc57bb210a
markox92 <159203552+markox92@users.noreply.github.com>
2024-09-10 16:05:18 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -13,14 +13,14 @@ sudo yay -S --noconfirm hyprland-git
 sudo pacman -S --noconfirm hyprpaper
 
 # Install package & software
-sudo pacman -S --noconfirm firefox alacritty kitty wget curl nano zip unzip thunar nautilus neofetch dunst cava
-yay -S --noconfirm rofi-lbonn-wayland-git waybar-git visual-studio-code-bin hyprlock-git 
+sudo pacman -S --noconfirm firefox alacritty kitty wget curl nano zip unzip nautilus neofetch dunst cava
+yay -S --noconfirm rofi-lbonn-wayland-git visual-studio-code-bin hyprlock-git 
 
 #sudo mv simple-sddm /usr/share/sddm/themes/
 function config_kernel() {
     printf "Config kernel..."
     sudo sed -Ei 's/^(MODULES=\([^\)]*)\)/\1nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
-    sudo echo -e "options nvidia-drm modeset=1 NVreg_RegistryDwords=\"PowerMizerEnable=0x1; PerfLevelSrc=0x2222; PowerMizerLevel=0x3; PowerMizerDefault=0x3; PowerMizerDefaultAC=0x3\"" | sudo tee -a /etc/modprobe.d/nvidia.conf
+    sudo echo -e "options nvidia_drm modeset=1 fbdev=1" | sudo tee -a /etc/modprobe.d/nvidia.conf
     sudo mkinitcpio -P
     if [ -f /etc/default/grub ]; then
         sudo sed -i 's/\(GRUB_CMDLINE_LINUX_DEFAULT=".*\)"/\1 nvidia_drm.modeset=1 nvidia.NVreg_PreserveVideoMemoryAllocations=1 nvidia.NVreg_EnableGpuFirmware=0 nvidia_drm.fbdev=1"/' /etc/default/grub
