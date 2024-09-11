e9829174827044633fca9485a11c2fd27b3cb77d
markox92 <159203552+markox92@users.noreply.github.com>
2024-09-11 17:15:12 +0200

    Create nvidia.sh

---
+++ nvidia.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo pacman -Syu && sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -rf yay
+yay -Syu --devel
+# Install NVIDIA Beta drivers.
+yay -S --noconfirm linux-headers 
+yay -S --noconfirm nvidia-beta-dkms libva-nvidia-driver-git
+
+function config_kernel() {
+    printf "Config kernel..."
+    sudo sed -Ei 's/^(MODULES=\([^\)]*)\)/\1nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+    sudo echo -e "options nvidia_drm modeset=1 fbdev=1" | sudo tee -a /etc/modprobe.d/nvidia.conf
+    sudo mkinitcpio -P
+    if [ -f /etc/default/grub ]; then
+        sudo sed -i 's/\(GRUB_CMDLINE_LINUX_DEFAULT=".*\)"/\1 nvidia_drm.modeset=1 nvidia_drm.fbdev=1"/' /etc/default/grub
+        sudo grub-mkconfig -o /boot/grub/grub.cfg
+    fi
+    printf "Done"
+}
+config_kernel
